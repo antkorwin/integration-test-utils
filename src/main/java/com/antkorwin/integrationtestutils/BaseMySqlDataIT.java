88bab7e041a1d87e5ef5a086ad56d62d602c9e8b
Anatoliy Korovin <antkorwin@gmail.com>
2018-07-14 16:28:19 +1000

    mysql bindings to test containers

--- src/main/java/com/antkorwin/integrationtestutils/BaseMySqlDataIT.java
+++ src/main/java/com/antkorwin/integrationtestutils/BaseMySqlDataIT.java
@@ -3,24 +3,25 @@ package com.antkorwin.integrationtestutils;
 import org.springframework.test.context.TestPropertySource;
 
 /**
- * Created by Korovin Anatolii on 14.07.2018.
+ * Created on 14.07.2018.
+ * <p>
+ * Run MySQL docker container for a testing DataJpa layer
+ * <p>
+ * Run only the JPA context!
+ * Does not load entire context configuration!
  *
- * Run an entire application context,
- * with PostgreSQL bindings to making integration tests with docker
- * (by test-containers)
- *
- * @author Korovin Anatolii
- * @version 1.0
+ * @author Korovin Anatoliy
  */
 @TestPropertySource(properties = {"spring.datasource.driver-class-name=org.testcontainers.jdbc.ContainerDatabaseDriver",
                                   "spring.datasource.type=com.zaxxer.hikari.HikariDataSource",
                                   "spring.datasource.username=user",
                                   "spring.datasource.password=password",
-                                  "spring.datasource.url=jdbc:tc:postgresql://hostname/databasename",
+                                  "spring.datasource.url=jdbc:tc:mysql://somehostname:port/databasename",
                                   "spring.jpa.generate-ddl=true",
                                   "spring.jpa.hibernate.ddl-auto=create-drop",
                                   "spring.jpa.show-sql=true",
                                   "spring.jpa.properties.hibernate.hbm2ddl.auto=create-drop",
-                                  "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.PostgreSQLDialect"})
-public class BasePostgresIT extends BaseIT {
+                                  "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQL5Dialect"})
+public abstract class BaseMySqlDataIT extends BaseDataIT {
+
 }
