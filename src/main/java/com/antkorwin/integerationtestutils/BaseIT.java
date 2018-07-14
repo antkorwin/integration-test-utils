5624d4480f5c5f6c12e3e68204936e68827867c5
Anatoliy Korovin <antkorwin@gmail.com>
2018-07-14 10:20:14 +1000

    BaseIT

---
+++ src/main/java/com/antkorwin/integerationtestutils/BaseIT.java
@@ -0,0 +1,18 @@
+package com.antkorwin.integerationtestutils;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by Korovin Anatolii on 14.07.2018.
+ *
+ * @author Korovin Anatolii
+ * @version 1.0
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class BaseIT {
+
+
+}
